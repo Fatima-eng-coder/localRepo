5de197625f57d6a29d21bb1b1bf8420eae1e1f22
Fatima Azaz <fatimaazaz1234@gmail.com>
2024-11-18 06:15:22 -0800

    Add Initial Files

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1=int(input("enter num1: "))
+num2=int(input("enter num2: "))
+a= (input("this can perform +,-,*,/,% \n enter operation"))
+
+if a=="+":
+    print(num1+num2)
+elif a=="-":
+    print(num1-num2)
+elif a=="*":
+    print(num1*num2)
+elif a=="/":
+    print(num1/num2)
+elif a=="%":
+    print(num1%num2)
